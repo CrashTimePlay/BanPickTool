7a59b2cc28d19bc04108c7813ec7f63bd16a191a
CrashTimePlay <abusecrash@gmail.com>
2021-01-24 23:52:31 +0600

    отслеживаемая веточка

--- src/api/actions/Read.php
+++ src/api/actions/Read.php
@@ -34,7 +34,7 @@ if($stmt !== null){
 else{
     http_response_code(404);
     echo json_encode(
-        array("message" => "No record foundededaaff.")
+        array("message" => "No record found.")
     );
 }
 ?>
